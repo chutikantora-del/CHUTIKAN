f3514777a4315e1ced96dff3b992ee68bbb29267
chutikantora-del <chutikan.tora@bumail.net>
2026-03-14 10:30:33 +0700

    Add files via upload

---
+++ data/foodMenu.js
@@ -0,0 +1,45 @@
+const foodMenu = [
+  {
+    name: "ข้าวไข่เจียว",
+    image: "/images/1.jpg",
+    nutrition: {
+      calories: 750,
+      protein: "30g",
+      carbs: "65g",
+      fat: "35g"
+    }
+  },
+    {
+    name: "ข้าวกระเพราไก่",
+    image: "/images/2.jpg",
+    nutrition: {
+      calories: 800,
+      protein: "20g",
+      carbs: "55g",
+      fat: "50g"
+    }
+  },
+    {
+    name: "ีปีกไก่ทอด",
+    image: "/images/3.jpg",
+    nutrition: {
+      calories: 700,
+      protein: "40g",
+      carbs: "60g",
+      fat: "35g"
+    }
+  },
+    {
+    name: "ข้าวคะน้าหมูไข่ดาว",
+    image: "/images/4.jpg",
+    nutrition: {
+      calories: 500,
+      protein: "30g",
+      carbs: "20g",
+      fat: "25g"
+    }
+  }
+];
+  
+
+module.exports = foodMenu;
